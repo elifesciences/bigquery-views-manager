bf823e0c605c91f00da9419835dfde124d5def14
HazalCiplak <hazalciplak@gmail.com>
2025-07-23 15:27:41 +0100

    for save_view_mapping fixed issue: Too many arguments for "append" of "list"

--- bigquery_views_manager/view_list.py
+++ bigquery_views_manager/view_list.py
@@ -83,11 +83,15 @@ def save_view_mapping(
     for view_template_name, view_dict in view_mapping.items():
         if is_materialized_view:
             file_content_as_list.append(
-                view_template_name + "," + view_dict.get(DATASET_NAME_KEY) + ".",
-                view_dict.get(VIEW_OR_TABLE_NAME_KEY),
+                view_template_name + "," +
+                view_dict.get(DATASET_NAME_KEY) + "." +
+                view_dict.get(VIEW_OR_TABLE_NAME_KEY)
             )
         else:
-            file_content_as_list.append(view_template_name + "," + view_dict.get(DATASET_NAME_KEY))
+            file_content_as_list.append(
+                view_template_name + "," +
+                view_dict.get(DATASET_NAME_KEY)
+            )
 
     file_content = "\n".join(file_content_as_list) + "\n"
     return Path(filename).write_text(file_content, encoding='utf-8')
