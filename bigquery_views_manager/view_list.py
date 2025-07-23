1f55a0b90471c996e11aa632698052ac280ad6ad
HazalCiplak <hazalciplak@gmail.com>
2025-07-23 15:09:37 +0100

    Fix type hint for is_materialized_view parameter in save_view_mapping function

--- bigquery_views_manager/view_list.py
+++ bigquery_views_manager/view_list.py
@@ -76,7 +76,7 @@ def create_simple_view_mapping_from_view_list(dataset: str,
 def save_view_mapping(
     filename: str,
     view_mapping: OrderedDict,
-    is_materialized_view: False
+    is_materialized_view: bool = False
 ):
     LOGGER.info("saving view mapping list to %s", filename)
     file_content_as_list = []
