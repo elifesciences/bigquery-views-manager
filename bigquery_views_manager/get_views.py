7a47c3464b19d15372d3cc091174c5397ce16b36
HazalCiplak <hazalciplak@gmail.com>
2025-07-28 15:22:51 +0100

    improved typing for get_views params

--- bigquery_views_manager/get_views.py
+++ bigquery_views_manager/get_views.py
@@ -4,9 +4,10 @@ from pathlib import Path
 
 from google.cloud import bigquery
 
+from bigquery_views_manager.materialize_views_typing import DatasetViewDataTypedDict
+
 from .views import get_bq_view_query, get_view_template_file
 from .view_template import ViewTemplate
-from .view_list import DATASET_NAME_KEY, VIEW_OR_TABLE_NAME_KEY
 
 LOGGER = logging.getLogger(__name__)
 
@@ -21,27 +22,27 @@ def get_view(  # pylint: disable=too-many-arguments
 ):
     bq_view_query = get_bq_view_query(client, view_name, dataset=dataset)
 
-    LOGGER.debug("bq_view_query(%s)=%r", view_name, bq_view_query)
+    LOGGER.debug('bq_view_query(%s)=%r', view_name, bq_view_query)
     view_template = ViewTemplate.from_query(bq_view_query, project=project).normalized
-    LOGGER.debug("view_template(%s)=%r", view_name, view_template)
+    LOGGER.debug('view_template(%s)=%r', view_name, view_template)
     view_template_file = get_view_template_file(base_dir, view_template_name)
     view_template.to_file(view_template_file)
-    LOGGER.info("updated %s", view_template_file)
+    LOGGER.info('updated %s', view_template_file)
 
 
 def get_views(
     client: bigquery.Client,
     base_dir: str | Path,
-    view_names_ordered_dict: OrderedDict,
+    view_names_ordered_dict: OrderedDict[str, DatasetViewDataTypedDict],
     project: str,
 ):
-    LOGGER.debug("view_names: %s", view_names_ordered_dict)
+    LOGGER.debug('view_names: %s', view_names_ordered_dict)
     for view_template_name, dataset_table_or_view_data in view_names_ordered_dict.items():
         get_view(
             client,
             base_dir,
-            dataset_table_or_view_data.get(VIEW_OR_TABLE_NAME_KEY),
+            dataset_table_or_view_data['table_name'],
             view_template_name,
             project=project,
-            dataset=dataset_table_or_view_data.get(DATASET_NAME_KEY),
+            dataset=dataset_table_or_view_data['dataset_name'],
         )
