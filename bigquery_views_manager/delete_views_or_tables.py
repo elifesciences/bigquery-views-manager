bedc86398c81d11adf76fd2495e7c98fc3617e1d
A. K <74007089+soggy-mushroom@users.noreply.github.com>
2025-07-28 15:04:36 +0100

    Improve typing of delete_views_or_tables

--- bigquery_views_manager/delete_views_or_tables.py
+++ bigquery_views_manager/delete_views_or_tables.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 from google.cloud.exceptions import NotFound
 from google.cloud import bigquery
 
-from .view_list import DATASET_NAME_KEY, VIEW_OR_TABLE_NAME_KEY
+from bigquery_views_manager.materialize_views_typing import DatasetViewDataTypedDict
 
 LOGGER = logging.getLogger(__name__)
 
@@ -36,19 +36,18 @@ def delete_views_or_table(
 
 def delete_views_or_tables(
     client: bigquery.Client,
-    view_template_to_table_name_mapping: OrderedDict
+    view_template_to_table_name_mapping: OrderedDict[str, DatasetViewDataTypedDict]
 ):
     LOGGER.debug("delete_views_or_tables: %s",
                  view_template_to_table_name_mapping)
     for _, dataset_view_or_table_data in view_template_to_table_name_mapping.items():
         if does_bigquery_table_exist(
                 client,
-                table_name=dataset_view_or_table_data.get(
-                    VIEW_OR_TABLE_NAME_KEY),
-                dataset_name=dataset_view_or_table_data.get(DATASET_NAME_KEY),
+                table_name=dataset_view_or_table_data["table_name"],
+                dataset_name=dataset_view_or_table_data["dataset_name"],
         ):
             delete_views_or_table(
                 client,
-                dataset_view_or_table_data.get(VIEW_OR_TABLE_NAME_KEY),
-                dataset=dataset_view_or_table_data.get(DATASET_NAME_KEY),
+                dataset_view_or_table_data["table_name"],
+                dataset=dataset_view_or_table_data["dataset_name"],
             )
