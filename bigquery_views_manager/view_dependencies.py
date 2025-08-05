4522eb1c11917dbce5c11bfce11514b41f79599e
HazalCiplak <hazalciplak@gmail.com>
2025-08-05 13:40:44 +0100

    created initial get_view_definition_map to extract query result

--- bigquery_views_manager/view_dependencies.py
+++ bigquery_views_manager/view_dependencies.py
@@ -12,14 +12,27 @@ def get_view_definition_query(
     )
 
 
-def get_view_dependencies(
+def get_view_definition_map(
     client: bigquery.Client,
     project: str,
     dataset: str
-) -> Mapping[str, Sequence[str]]:
+) -> Mapping[str, str]:
     query_job = client.query(get_view_definition_query(
         project=project,
         dataset=dataset
     ))
     query_job.result()
+    return {}
+
+
+def get_view_dependencies(
+    client: bigquery.Client,
+    project: str,
+    dataset: str
+) -> Mapping[str, Sequence[str]]:
+    get_view_definition_map(
+        client=client,
+        project=project,
+        dataset=dataset
+    )
     return {}
